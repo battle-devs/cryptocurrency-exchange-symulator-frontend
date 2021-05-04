6cfc6b53e6e9b2a43a1c461c47158e2efc4f4e05
ewe.prochownik@gmail.com <R433Y7tw8>
2021-05-04 20:47:18 +0200

    initial commit

---
+++ apps/cryptocurrency-exchange-symulator/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'inzynieria-oprogramowania-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'cryptocurrency-exchange-symulator';
+}
