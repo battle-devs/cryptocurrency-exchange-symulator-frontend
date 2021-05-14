784551c3ccae351a641e16456262f8af22268970
ewe.prochownik@gmail.com <ewe.prochownik@gmail.com>
2021-05-14 19:50:44 +0200

    Add initial home page architecture

---
+++ apps/cryptocurrency-exchange-symulator/src/app/features/home-page/home-page.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './home-page/home-page.component';
+import { BannerComponent } from './home-page/banner/banner.component';
+import { CurrencyListComponent } from './home-page/currency-list/currency-list.component';
+import { CurrencyComponent } from './home-page/currency-list/currency/currency.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomePageComponent,
+    BannerComponent,
+    CurrencyListComponent,
+    CurrencyComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomePageModule { }
