20a99ebbf03f009e342a3cae8ce1f660bc01295a
ewe.prochownik@gmail.com <ewe.prochownik@gmail.com>
2021-05-28 15:50:50 +0200

    Change currency type

--- apps/cryptocurrency-exchange-simulator/src/app/features/home-page/models/cryptocurrency.data.ts
+++ apps/cryptocurrency-exchange-simulator/src/app/features/home-page/models/cryptocurrency.data.ts
@@ -5,12 +5,12 @@ export interface CryptocurrencyBlock {
 }
 
 export const cryptocurrencies: CryptocurrencyBlock[] = [
-  { name: 'BTC', currency: 'USD', amount: 38933.81 },
-  { name: 'ETH', currency: 'USD', amount: 2767.4 },
-  { name: 'BCH', currency: 'USD', amount: 769.36 },
-  { name: 'LTC', currency: 'USD', amount: 197.56 },
-  { name: 'XLM', currency: 'USD', amount: 0.43 },
-  { name: 'ETC', currency: 'USD', amount: 74.67 },
-  { name: 'EOS', currency: 'USD', amount: 7.11 },
-  { name: 'XTZ', currency: 'USD', amount: 3.94 },
+  { name: 'BTC', currency: '$', amount: 38933.81 },
+  { name: 'ETH', currency: '$', amount: 2767.4 },
+  { name: 'BCH', currency: '$', amount: 769.36 },
+  { name: 'LTC', currency: '$', amount: 197.56 },
+  { name: 'XLM', currency: '$', amount: 0.43 },
+  { name: 'ETC', currency: '$', amount: 74.67 },
+  { name: 'EOS', currency: '$', amount: 7.11 },
+  { name: 'XTZ', currency: '$', amount: 3.94 },
 ];
