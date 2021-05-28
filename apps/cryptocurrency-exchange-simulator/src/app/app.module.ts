521f3cd25fd7430b60dc462801da9299429e8aef
ewe.prochownik@gmail.com <ewe.prochownik@gmail.com>
2021-05-28 21:41:41 +0200

    Add banner content

--- apps/cryptocurrency-exchange-simulator/src/app/app.module.ts
+++ apps/cryptocurrency-exchange-simulator/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const routes: Routes = [
   {
@@ -21,7 +22,7 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  imports: [BrowserModule, RouterModule.forRoot(routes), BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
