cf62f6ddfe30791d48dc359574330b24be3255a6
ewe.prochownik@gmail.com <ewe.prochownik@gmail.com>
2021-06-26 17:07:03 +0200

    Added progress bar

--- apps/cryptocurrency-exchange-simulator/src/app/app.component.ts
+++ apps/cryptocurrency-exchange-simulator/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component, Output } from '@angular/core';
+import { Component } from '@angular/core';
+import { ProgressBarService } from './core/services/progress-bar.service';
 
 @Component({
   selector: 'inzynieria-oprogramowania-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  providers: [ProgressBarService],
 })
 export class AppComponent {
   title = 'cryptocurrency-exchange-simulator';
+  public readonly progressBarVisible$ = this._progressBarService.visible$;
+  constructor(private readonly _progressBarService: ProgressBarService) {}
 }
