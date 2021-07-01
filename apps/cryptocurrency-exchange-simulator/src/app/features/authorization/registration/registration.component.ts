5b4d86acae0f27579fec3d5f6a7dad1b744e4d29
ewe.prochownik@gmail.com <ewe.prochownik@gmail.com>
2021-07-01 21:26:08 +0200

    Added register view

--- apps/cryptocurrency-exchange-simulator/src/app/features/authorization/registration/registration.component.ts
+++ apps/cryptocurrency-exchange-simulator/src/app/features/authorization/registration/registration.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RegistrationForm } from '../models/registration';
+import { FormGroupConfig } from '../../../core/services/form-group-config.type';
 
 @Component({
   selector: 'inzynieria-oprogramowania-registration',
@@ -7,6 +9,8 @@ import { FormBuilder } from '@angular/forms';
   styleUrls: ['./registration.component.scss'],
 })
 export class RegistrationComponent implements OnInit {
+  public registrationForm: FormGroup;
+
   constructor(private readonly _fb: FormBuilder) {}
 
   ngOnInit(): void {
@@ -14,6 +18,54 @@ export class RegistrationComponent implements OnInit {
   }
 
   private _createForm = () => {
-    const config: FormGroupConfig;
+    const config: FormGroupConfig<RegistrationForm> = {
+      firstName: [
+        '',
+        [
+          Validators.required,
+          Validators.maxLength(255),
+          Validators.minLength(3),
+        ],
+      ],
+      lastName: [
+        '',
+        [
+          Validators.required,
+          Validators.maxLength(255),
+          Validators.minLength(3),
+        ],
+      ],
+      userName: [
+        '',
+        [
+          Validators.required,
+          Validators.maxLength(255),
+          Validators.minLength(3),
+        ],
+      ],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.maxLength(255),
+          Validators.minLength(3),
+          Validators.email,
+        ],
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.maxLength(255),
+          Validators.minLength(3),
+          Validators.pattern('(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,}'),
+        ],
+      ],
+      user_password: ['', []],
+    };
+
+    this.registrationForm = this._fb.group(config);
   };
+
+  public register() {}
 }
