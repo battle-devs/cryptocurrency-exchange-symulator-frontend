7e67ac9e550d9c1687721a294c0c55b127971955
ewe.prochownik@gmail.com <ewe.prochownik@gmail.com>
2021-07-01 22:35:52 +0200

    Added login component

--- apps/cryptocurrency-exchange-simulator/src/app/features/authorization/authorization.module.ts
+++ apps/cryptocurrency-exchange-simulator/src/app/features/authorization/authorization.module.ts
@@ -2,20 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '../../shared/shared.module';
 import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
     path: 'registration',
     component: RegistrationComponent,
   },
-  // {
-  //   path: 'login',
-  //   component: LoginComponent
-  // },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
 ];
 
 @NgModule({
-  declarations: [RegistrationComponent],
+  declarations: [RegistrationComponent, LoginComponent],
   imports: [SharedModule, RouterModule.forChild(routes)],
 })
 export class AuthorizationModule {}
